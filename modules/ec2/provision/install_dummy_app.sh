9d1d0a112a0980dadcb8ce74a9859467b31f46c0
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2017-09-08 09:51:32 -0500

    fix add nvm to path when install the app

--- modules/ec2/provision/install_dummy_app.sh
+++ modules/ec2/provision/install_dummy_app.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# This loads nvm
+export NVM_DIR="$HOME/.nvm"
+\. "$NVM_DIR/nvm.sh"
+
 cp -r $HOME/provision/app $HOME/
 cp -r $HOME/provision/pm2.json $HOME/app-pm2.json
 npm i -g pm2
